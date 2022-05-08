6aa584e81a0cfcf76f214c67ad0460ef9eef5e6f
haidil <mohamadhaidilidris@hotmail.com>
2022-05-09 06:14:23 +0800

    <

--- routes/storage.js
+++ routes/storage.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const {getAllStorageData, updatePinNumbers, putPIN, validatePIN} = require('../controller/storage')
+const {getAllStorageData, updatePinNumbers, putPIN, validatePIN, motionDetected} = require('../controller/storage')
 
 router.route('/').get(getAllStorageData)
 router.route('/setpin').get(putPIN)
 router.route('/:id').get(updatePinNumbers)
 router.route('/validatepin').post(validatePIN)
+router.route('/motiondetect').post(motionDetected)
 
 module.exports = router
